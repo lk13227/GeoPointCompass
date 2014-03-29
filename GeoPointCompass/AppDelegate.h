6cc3609f996bd8582f6f71b67d450614ef09fbac
Maduranga Edirisinghe <maduranga55@gmail.com>
2014-03-29 20:54:44 +0530

    Initial Commit

---
+++ GeoPointCompass/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GeoPointCompass
+//
+//  Created by Maduranga Edirisinghe on 3/29/14.
+//  Copyright (c) 2014 Maduranga Edirisinghe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GeoPointCompassTests/GeoPointCompassTests.m
@@ -0,0 +1,34 @@
+//
+//  GeoPointCompassTests.m
+//  GeoPointCompassTests
+//
+//  Created by Maduranga Edirisinghe on 3/29/14.
+//  Copyright (c) 2014 Maduranga Edirisinghe. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GeoPointCompassTests : XCTestCase
+
+@end
+
+@implementation GeoPointCompassTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
